082731fc21641a56687d1c579d0f697caa211292
jiwonan <s2018s31@e-mirim.hs.kr>
2020-05-19 11:35:42 +0900

    Button Test 코드에서 버튼 생성

--- FirstFormApplication/Form1.Designer.cs
+++ FirstFormApplication/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.myButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // myButton
+            // 
+            this.myButton.Location = new System.Drawing.Point(12, 12);
+            this.myButton.Name = "myButton";
+            this.myButton.Size = new System.Drawing.Size(75, 23);
+            this.myButton.TabIndex = 0;
+            this.myButton.Text = "안지원";
+            this.myButton.UseVisualStyleBackColor = true;
+            this.myButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button myButton;
     }
 }
 
--- FirstFormApplication/Form1.cs
+++ FirstFormApplication/Form1.cs
@@ -15,6 +15,31 @@ namespace FirstFormApplication
         public Form1()
         {
             InitializeComponent();
+
+            myButton.Enabled = true;
+            myButton.Text = "코드에서 변경!";
+            myButton.Width = 200;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Button button = new Button();
+                Controls.Add(button);
+
+                button.Location = new Point(213, 13 + (23 + 3) * i);
+                button.Text = "동적 생성 " + i + "번째";
+                button.Width = 100;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
